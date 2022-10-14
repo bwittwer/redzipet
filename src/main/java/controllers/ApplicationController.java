d1dbef462f492b5d46e647472753b3715304f32f
Bernard Wittwer <bwittwer@users.noreply.github.com>
2022-10-14 14:22:24 +0200

    Update ApplicationController.java

--- src/main/java/controllers/ApplicationController.java
+++ src/main/java/controllers/ApplicationController.java
@@ -41,7 +41,7 @@ public class ApplicationController {
 	public Result helloWorldJson() {
 
 		SimplePojo simplePojo = new SimplePojo();
-		simplePojo.content = "Hello World! Hello Json!";
+		simplePojo.content = "Hello World! Hello Json! Modified for testing";
 
 
 
